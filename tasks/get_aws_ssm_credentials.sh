a8283aee13c0a649155eafcd356dbea49506b99a
Benjamin Robertson <ben.robertson@puppet.com>
2022-03-02 11:21:12 +1100

    update

--- tasks/get_aws_ssm_credentials.sh
+++ tasks/get_aws_ssm_credentials.sh
@@ -3,6 +3,6 @@
 echo "my token is"
 echo $PT_token
 
-activation_code = `/usr/local/bin/aws-get | grep Value | awk '{ print $36 }' | sed 's/\\n\\nActivation//'`
+activation_code=`/usr/local/bin/aws-get | grep Value | awk '{ print $36 }' | sed 's/\\n\\nActivation//'`
 echo "my activation code is"
 echo $activation_code
